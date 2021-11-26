3b918a21c99af567c1d063ab84a5311092f1ec3f
Hyungjun Lim <sculd3@gmail.com>
2021-11-26 13:10:30 -0500

    hello_world

--- cloud_function.py
+++ cloud_function.py
@@ -47,3 +47,16 @@ def hello_pubsub(event, context):
     """
     print(event)
     delete_pods()
+
+def hello_world(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    print(request_json)
+    delete_pods()
